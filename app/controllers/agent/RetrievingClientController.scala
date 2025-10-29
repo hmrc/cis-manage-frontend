d8c6f0fced2a8defa139991fed6ff4081c5363d8
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2025-10-29 13:06:17 +0000

    [DTR-003] - update configs, apply scalafmt

--- app/controllers/agent/RetrievingClientController.scala
+++ app/controllers/agent/RetrievingClientController.scala
@@ -23,14 +23,15 @@ import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.agent.RetrievingClientView
 import javax.inject.Inject
 
-class RetrievingClientController @Inject()(
+class RetrievingClientController @Inject() (
   override val messagesApi: MessagesApi,
   val controllerComponents: MessagesControllerComponents,
   view: RetrievingClientView
 )(implicit appConfig: FrontendAppConfig)
-    extends FrontendBaseController with I18nSupport {
+    extends FrontendBaseController
+    with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
-      Ok(view())
+    Ok(view())
   }
 }
