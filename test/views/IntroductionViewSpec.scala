914f07d98515aa45bdfc8c5b93e06e2d25c22ae1
Richy Jassal <20478717+jassalrichy@users.noreply.github.com>
2025-11-05 07:57:09 +0000

    [DTR-686] - apply scalafmt

--- test/views/IntroductionViewSpec.scala
+++ test/views/IntroductionViewSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views
 
 import base.SpecBase
@@ -29,7 +45,7 @@ class IntroductionViewSpec extends SpecBase {
       doc.select("li").text must include(messages("introduction.listItem6"))
       doc.select("li").text must include(messages("introduction.listItem7"))
 
-      doc.select("p").text  must include(messages("introduction.inset"))
+      doc.select("p").text must include(messages("introduction.inset"))
 
       doc.select("h2").text must include(messages("introduction.h2"))
       doc.select("p").text  must include(messages("introduction.p2"))
@@ -37,10 +53,10 @@ class IntroductionViewSpec extends SpecBase {
       doc.select("li").text must include(messages("introduction.listItem9"))
       doc.select("li").text must include(messages("introduction.listItem10"))
 
-      doc.select("span").text must include(messages("introduction.details.summary"))
-      doc.select("h3").text   must include(messages("introduction.details.h3"))
-      doc.select("p").text    must include(messages("introduction.details.p1"))
-      doc.select("p").text    must include(messages("introduction.details.p2"))
+      doc.select("span").text                   must include(messages("introduction.details.summary"))
+      doc.select("h3").text                     must include(messages("introduction.details.h3"))
+      doc.select("p").text                      must include(messages("introduction.details.p1"))
+      doc.select("p").text                      must include(messages("introduction.details.p2"))
       doc.getElementsByClass("govuk-link").text must include(messages("introduction.details.link"))
 
       doc.getElementsByClass("govuk-button").text must include(messages("introduction.button.continue"))
