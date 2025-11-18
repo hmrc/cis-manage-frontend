ff5ed4700de9219bf7c2cb61a6442dfba4551e19
samraducan <231910514+samraducan@users.noreply.github.com>
2025-11-18 14:14:15 +0000

    DTR-1046: Add continue button

--- app/controllers/CheckSubcontractorRecordsController.scala
+++ app/controllers/CheckSubcontractorRecordsController.scala
@@ -23,13 +23,14 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.CheckSubcontractorRecordsView
 
-class CheckSubcontractorRecordsController @Inject()(
+class CheckSubcontractorRecordsController @Inject() (
   override val messagesApi: MessagesApi,
   val controllerComponents: MessagesControllerComponents,
   view: CheckSubcontractorRecordsView
-) extends FrontendBaseController with I18nSupport {
+) extends FrontendBaseController
+    with I18nSupport {
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
-      Ok(view())
+    Ok(view())
   }
 }
