83dec9ddf0108e54261a20d79fdbd2d40837c53a
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2025-12-11 13:53:49 +0000

    [DTR-1290]  CIS Screen NLP1 - Notices Landing Page (#38)
    * [DTR-1290] Adding scaffold page
    
    * [DTR-1290] Fix ambiguous ButtonViewModel import and refactor ManageNotice
    
    * [DTR-1290] Content changes
    
    * [DTR-1290] Adding unit tests
    
    * [DTR-1290] Changing folder structure
    
    * [DTR-1290] Content changes
    
    * [DTR-1290] Fixing test

---
+++ app/viewmodels/notices/ManageNoticesStatementsViewModel.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.notices
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
+import uk.gov.hmrc.govukfrontend.views.viewmodels.select.SelectItem
+
+case class ManageNoticesStatementsRowViewModel(
+  date: String,
+  noticeType: String,
+  description: String,
+  status: String,
+  statusColour: String,
+  actionUrl: String
+)
+
+case class ManageNoticesStatementsPageViewModel(
+  noticeTypeItems: Seq[SelectItem],
+  readStatusItems: Seq[SelectItem],
+  dateRangeItems: Seq[RadioItem],
+  notices: Seq[ManageNoticesStatementsRowViewModel],
+  totalRecords: Int
+)
+
+object ManageNoticesStatementsPageViewModel {
+
+  def apply(
+    notices: Seq[ManageNoticesStatementsRowViewModel]
+  )(implicit messages: Messages): ManageNoticesStatementsPageViewModel =
+    apply(notices, notices.size)
+
+  def apply(
+    notices: Seq[ManageNoticesStatementsRowViewModel],
+    totalRecords: Int
+  )(implicit messages: Messages): ManageNoticesStatementsPageViewModel =
+    ManageNoticesStatementsPageViewModel(
+      noticeTypeItems = Seq(
+        SelectItem(
+          value = Some("all"),
+          text = messages("manageNoticesStatements.noticeType.all")
+        ),
+        SelectItem(
+          value = Some("confirmationStatements"),
+          text = messages("manageNoticesStatements.noticeType.confirmationStatements")
+        ),
+        SelectItem(
+          value = Some("penaltyWarnings"),
+          text = messages("manageNoticesStatements.noticeType.penaltyWarnings")
+        ),
+        SelectItem(
+          value = Some("general"),
+          text = messages("manageNoticesStatements.noticeType.general")
+        )
+      ),
+      readStatusItems = Seq(
+        SelectItem(
+          value = Some("all"),
+          text = messages("manageNoticesStatements.status.all")
+        ),
+        SelectItem(
+          value = Some("read"),
+          text = messages("manageNoticesStatements.status.read")
+        ),
+        SelectItem(
+          value = Some("unread"),
+          text = messages("manageNoticesStatements.status.unread")
+        )
+      ),
+      dateRangeItems = Seq(
+        RadioItem(
+          value = Some("last7Days"),
+          content = Text(messages("manageNoticesStatements.dateFilter.last7Days"))
+        ),
+        RadioItem(
+          value = Some("lastMonth"),
+          content = Text(messages("manageNoticesStatements.dateFilter.lastMonth"))
+        ),
+        RadioItem(
+          value = Some("dateRange"),
+          content = Text(messages("manageNoticesStatements.dateFilter.dateRange"))
+        ),
+        RadioItem(
+          value = Some("all"),
+          content = Text(messages("manageNoticesStatements.dateFilter.all"))
+        )
+      ),
+      notices = notices,
+      totalRecords = totalRecords
+    )
+}
---
+++ test/viewmodels/notices/ManageNoticesStatementsPageViewModelSpec.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.notices
+
+import base.SpecBase
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+
+class ManageNoticesStatementsPageViewModelSpec extends SpecBase {
+
+  private val app      = applicationBuilder().build()
+  given msgs: Messages = messages(app)
+
+  "ManageNoticesStatementsPageViewModel" - {
+
+    "should build notice type items with All first and no default selection" in {
+      val viewModel = ManageNoticesStatementsPageViewModel(Seq.empty, totalRecords = 0)
+
+      assert(
+        viewModel.noticeTypeItems.map(_.value) == Seq(
+          Some("all"),
+          Some("confirmationStatements"),
+          Some("penaltyWarnings"),
+          Some("general")
+        )
+      )
+
+      assert(
+        viewModel.noticeTypeItems.map(_.text) == Seq(
+          msgs("manageNoticesStatements.noticeType.all"),
+          msgs("manageNoticesStatements.noticeType.confirmationStatements"),
+          msgs("manageNoticesStatements.noticeType.penaltyWarnings"),
+          msgs("manageNoticesStatements.noticeType.general")
+        )
+      )
+
+      viewModel.noticeTypeItems.foreach(item => assert(item.selected == false))
+    }
+
+    "should build read status items with no default selection" in {
+      val viewModel = ManageNoticesStatementsPageViewModel(Seq.empty, totalRecords = 0)
+
+      val texts      = viewModel.readStatusItems.map(_.text)
+      val values     = viewModel.readStatusItems.map(_.value)
+      val selections = viewModel.readStatusItems.map(_.selected)
+
+      assert(
+        texts == Seq(
+          msgs("manageNoticesStatements.status.all"),
+          msgs("manageNoticesStatements.status.read"),
+          msgs("manageNoticesStatements.status.unread")
+        )
+      )
+      assert(values == Seq(Some("all"), Some("read"), Some("unread")))
+      selections.foreach(sel => assert(sel == false))
+    }
+
+    "should build date range items with none checked" in {
+      val viewModel = ManageNoticesStatementsPageViewModel(Seq.empty, totalRecords = 0)
+
+      val values   = viewModel.dateRangeItems.map(_.value)
+      val contents = viewModel.dateRangeItems.map(_.content)
+      val checked  = viewModel.dateRangeItems.map(_.checked)
+
+      assert(values == Seq(Some("last7Days"), Some("lastMonth"), Some("dateRange"), Some("all")))
+      assert(
+        contents == Seq(
+          Text(msgs("manageNoticesStatements.dateFilter.last7Days")),
+          Text(msgs("manageNoticesStatements.dateFilter.lastMonth")),
+          Text(msgs("manageNoticesStatements.dateFilter.dateRange")),
+          Text(msgs("manageNoticesStatements.dateFilter.all"))
+        )
+      )
+      checked.foreach(isChecked => assert(isChecked == false))
+    }
+
+    "should include notices and total record count" in {
+      val notices = Seq(
+        ManageNoticesStatementsRowViewModel("2025-01-01", "confirmationStatements", "desc1", "read", "green", "#1"),
+        ManageNoticesStatementsRowViewModel("2025-01-02", "penaltyWarnings", "desc2", "unread", "red", "#2")
+      )
+
+      val viewModel = ManageNoticesStatementsPageViewModel(notices, totalRecords = 5)
+
+      assert(viewModel.notices == notices)
+      assert(viewModel.totalRecords == 5)
+    }
+  }
+}
