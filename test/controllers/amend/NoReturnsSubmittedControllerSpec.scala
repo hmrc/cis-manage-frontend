9ab76a345194c809d8383e64a1262b3aa20c1a50
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2026-04-17 14:48:51 +0100

    DTR-4540: controller tests

--- test/controllers/amend/NoReturnsSubmittedControllerSpec.scala
+++ test/controllers/amend/NoReturnsSubmittedControllerSpec.scala
@@ -17,10 +17,9 @@
 package controllers.amend
 
 import base.SpecBase
-import controllers.routes
 import play.api.test.FakeRequest
 import play.api.test.Helpers.*
-import views.html.NoReturnsSubmittedView
+import views.html.amend.NoReturnsSubmittedView
 
 class NoReturnsSubmittedControllerSpec extends SpecBase {
 
@@ -28,17 +27,34 @@ class NoReturnsSubmittedControllerSpec extends SpecBase {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      val application = applicationBuilder(userAnswers = Some(userAnswersWithCisId)).build()
 
       running(application) {
-        val request = FakeRequest(GET, routes.NoReturnsSubmittedController.onPageLoad().url)
+        val request = FakeRequest(GET, controllers.amend.routes.NoReturnsSubmittedController.onPageLoad().url)
 
         val result = route(application, request).value
 
         val view = application.injector.instanceOf[NoReturnsSubmittedView]
 
         status(result) mustEqual OK
-        contentAsString(result) mustEqual view()(request, messages(application)).toString
+        contentAsString(result) mustEqual view("1")(request, messages(application)).toString
+      }
+    }
+
+    "throw IllegalStateException when cisId is missing" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+
+        val request = FakeRequest(GET, controllers.amend.routes.NoReturnsSubmittedController.onPageLoad().url)
+
+        val controller = application.injector.instanceOf[NoReturnsSubmittedController]
+
+        val exception = controller.onPageLoad()(request).failed.futureValue
+
+        exception mustBe a[IllegalStateException]
+        exception.getMessage mustBe "cisId missing from userAnswers"
       }
     }
   }
