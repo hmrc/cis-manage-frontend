fced64d040341e8baa4ed84799608f396748b02d
Yohan Jayarathna <129070827+yohan-oc@users.noreply.github.com>
2026-04-21 12:11:44 +0100

    DTR-4540: AMR Screen Ticket MRAR10 - No submitted returns (#64)

---
+++ test/views/amend/NoReturnsSubmittedViewSpec.scala
@@ -0,0 +1,41 @@
+package views.amend
+
+import base.SpecBase
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.Application
+import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import play.twirl.api.HtmlFormat
+import views.html.amend.NoReturnsSubmittedView
+
+class NoReturnsSubmittedViewSpec extends SpecBase {
+
+  trait Setup {
+
+    "must render the page with the correct content" in new Setup {
+      val html: HtmlFormat.Appendable = view(cisId = "1")
+      val doc: Document               = Jsoup.parse(html.toString)
+
+      doc.title             must include(messages("amend.noReturnsSubmitted.title"))
+      doc.select("h1").text must include(messages("amend.noReturnsSubmitted.heading"))
+
+      doc.select("p.govuk-body").text must include(messages("amend.noReturnsSubmitted.p1"))
+
+      doc.select("p.govuk-body").text must include(messages("amend.noReturnsSubmitted.p2"))
+      doc.select("a.govuk-link").text must include(messages("amend.noReturnsSubmitted.p2.link"))
+      doc.select("p.govuk-body").text must include(messages("amend.noReturnsSubmitted.p3"))
+      doc.select("a.govuk-link").text must include(messages("amend.noReturnsSubmitted.p3.links"))
+    }
+
+    val app: Application             = applicationBuilder().build()
+    val view: NoReturnsSubmittedView = app.injector.instanceOf[NoReturnsSubmittedView]
+
+    implicit val request: play.api.mvc.Request[_] = FakeRequest()
+    implicit val messages: Messages               =
+      play.api.i18n.MessagesImpl(
+        play.api.i18n.Lang.defaultLang,
+        app.injector.instanceOf[play.api.i18n.MessagesApi]
+      )
+  }
+}
