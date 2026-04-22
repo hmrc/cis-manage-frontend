bb301b943432198c4d4a6f73c7e3804255151124
samraducan <231910514+samraducan@users.noreply.github.com>
2026-04-22 12:14:36 +0100

    DTR-4537: Run sbt formatAll

--- test/controllers/ReturnsLandingControllerSpec.scala
+++ test/controllers/ReturnsLandingControllerSpec.scala
@@ -37,7 +37,7 @@ class ReturnsLandingControllerSpec extends SpecBase with MockitoSugar {
   private val context = ReturnsLandingContext(
     contractorName = "ABC Construction Ltd",
     standardReturnLink = "/standard-link",
-    nilReturnLink = "/nil-link",
+    nilReturnLink = "/nil-link"
   )
 
   "ReturnsLandingController.onPageLoad" - {
