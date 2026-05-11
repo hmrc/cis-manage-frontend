8f0697fb5223cc903db1bb743a38f36889bf51e1
yanshengzhangHMRC <171041010+yanshengzhangHMRC@users.noreply.github.com>
2026-05-11 10:02:59 +0100

    [DTR-5199] PR comments

--- test/controllers/history/PrintSubmissionDetailsControllerSpec.scala
+++ test/controllers/history/PrintSubmissionDetailsControllerSpec.scala
@@ -41,7 +41,8 @@ class PrintSubmissionDetailsControllerSpec extends SpecBase with MockitoSugar {
 
     "must return OK and the correct view for a GET" in {
 
-      val mockManageService = mock[ManageService]
+      val mockManageService       = mock[ManageService]
+      val submittedReturnsService = mock[SubmittedReturnsService]
 
       val mockResponse = GetSubmittedMonthlyReturnsDataResponse(
         scheme = SubmittedSchemeData("Scheme Name", "163", "AB0063"),
@@ -52,7 +53,7 @@ class PrintSubmissionDetailsControllerSpec extends SpecBase with MockitoSugar {
         monthlyReturnItems = Seq.empty,
         submission = SubmittedSubmissionData(
           submissionId = 10L,
-          submissionType = Some("Original"),
+          submissionType = Some("MONTHLY_RETURN"),
           activeObjectId = Some(20L),
           status = "Accepted",
           hmrcMarkGenerated = Some("mark1"),
@@ -62,45 +63,28 @@ class PrintSubmissionDetailsControllerSpec extends SpecBase with MockitoSugar {
         )
       )
 
-      when(
-        mockManageService.getSubmittedMonthlyReturnsData(eqTo("1"), eqTo(2026), eqTo(4), eqTo("N"))(any[HeaderCarrier])
-      )
-        .thenReturn(Future.successful(mockResponse))
-
-      val submittedReturnsService = mock[SubmittedReturnsService]
-
-      val monthYear              = "April 2026"
-      val submittedTime          = "8:46am"
-      val submittedDate          = "16 March 2025"
-      val receiptReferenceNumber = "6QEDAHDREBY455GDNCPMDCNDFBDBJSJSJDNDDHDJDZ5"
-      val submissionType         = "standard"
-      val contractorName         = "PAL 355 Scheme"
-      val payeReference          = "123/AB456"
-      val totalPaymentsMade      = "£1900"
-      val totalCostOfMaterials   = "£616"
-      val totalTaxDeducted       = "£380"
-
-      val subcontractors = Seq(
-        SubcontractorPayment("BuildRight Construction", "£165", "£95", "£95"),
-        SubcontractorPayment("Northern Trades Ltd", "£75", "£55", "£55"),
-        SubcontractorPayment("TyneWear Ltd", "£165", "£125", "£55")
-      )
-
       val model = SubmittedReturnPrintViewModel(
-        monthYear = monthYear,
-        submittedTime = submittedTime,
-        submittedDate = submittedDate,
-        receiptReferenceNumber = receiptReferenceNumber,
-        submissionType = submissionType,
-        contractorName = contractorName,
-        payeReference = payeReference,
-        totalPaymentsMade = totalPaymentsMade,
-        totalCostOfMaterials = totalCostOfMaterials,
-        totalTaxDeducted = totalTaxDeducted,
-        subcontractors = subcontractors
+        monthYear = "April 2026",
+        submittedTime = "8:46am",
+        submittedDate = "16 March 2025",
+        receiptReferenceNumber = "6QEDAHDREBY455GDNCPMDCNDFBDBJSJSJDNDDHDJDZ5",
+        submissionType = "standard",
+        contractorName = "PAL 355 Scheme",
+        payeReference = "123/AB456",
+        totalPaymentsMade = "£1900",
+        totalCostOfMaterials = "£616",
+        totalTaxDeducted = "£380",
+        subcontractors = Seq.empty
       )
 
-      when(submittedReturnsService.buildSubmittedReturnPrintViewModel(any(), any())).thenReturn(model)
+      when(
+        mockManageService.getSubmittedMonthlyReturnsData(eqTo("1"), eqTo(2026), eqTo(4), eqTo("N"))(
+          any[HeaderCarrier]
+        )
+      ).thenReturn(Future.successful(mockResponse))
+
+      when(submittedReturnsService.buildSubmittedReturnPrintViewModel(any(), any()))
+        .thenReturn(model)
 
       val application = applicationBuilder(userAnswers = Some(userAnswersWithCisId))
         .overrides(
@@ -111,12 +95,85 @@ class PrintSubmissionDetailsControllerSpec extends SpecBase with MockitoSugar {
 
       running(application) {
         val request = FakeRequest(GET, printSubmissionDetailsRoute)
+        val result  = route(application, request).value
+        val view    = application.injector.instanceOf[PrintSubmissionDetailsView]
 
-        val result = route(application, request).value
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view(
+          model,
+          controllers.history.routes.SubmittedReturnsController.onPageLoadAllYears().url
+        )(request, messages(application)).toString
+      }
+    }
 
-        val view = application.injector.instanceOf[PrintSubmissionDetailsView]
+    "must return OK and the correct view for a GET when from is single" in {
 
-        val historyUrl = controllers.history.routes.SubmittedReturnsController.onPageLoadAllYears().url
+      val routeWithSingleFrom =
+        controllers.history.routes.PrintSubmissionDetailsController
+          .onPageLoad(2026, 4, "N", "single")
+          .url
+
+      val mockManageService       = mock[ManageService]
+      val submittedReturnsService = mock[SubmittedReturnsService]
+
+      val mockResponse = GetSubmittedMonthlyReturnsDataResponse(
+        scheme = SubmittedSchemeData("Scheme Name", "163", "AB0063"),
+        monthlyReturnId = 3000L,
+        taxYear = 2026,
+        taxMonth = 4,
+        nilReturnIndicator = "Y",
+        monthlyReturnItems = Seq.empty,
+        submission = SubmittedSubmissionData(
+          submissionId = 10L,
+          submissionType = Some("MONTHLY_RETURN"),
+          activeObjectId = Some(20L),
+          status = "Accepted",
+          hmrcMarkGenerated = Some("mark1"),
+          hmrcMarkGgis = Some("ggis1"),
+          emailRecipient = Some("test@example.com"),
+          acceptedTime = Some(Instant.now())
+        )
+      )
+
+      val model = SubmittedReturnPrintViewModel(
+        monthYear = "April 2026",
+        submittedTime = "8:46am",
+        submittedDate = "16 March 2025",
+        receiptReferenceNumber = "6QEDAHDREBY455GDNCPMDCNDFBDBJSJSJDNDDHDJDZ5",
+        submissionType = "standard",
+        contractorName = "PAL 355 Scheme",
+        payeReference = "123/AB456",
+        totalPaymentsMade = "£1900",
+        totalCostOfMaterials = "£616",
+        totalTaxDeducted = "£380",
+        subcontractors = Seq.empty
+      )
+
+      when(
+        mockManageService.getSubmittedMonthlyReturnsData(eqTo("1"), eqTo(2026), eqTo(4), eqTo("N"))(
+          any[HeaderCarrier]
+        )
+      ).thenReturn(Future.successful(mockResponse))
+
+      when(submittedReturnsService.buildSubmittedReturnPrintViewModel(any(), any()))
+        .thenReturn(model)
+
+      val application = applicationBuilder(userAnswers = Some(userAnswersWithCisId))
+        .overrides(
+          bind[ManageService].toInstance(mockManageService),
+          bind[SubmittedReturnsService].toInstance(submittedReturnsService)
+        )
+        .build()
+
+      running(application) {
+        val request = FakeRequest(GET, routeWithSingleFrom)
+        val result  = route(application, request).value
+        val view    = application.injector.instanceOf[PrintSubmissionDetailsView]
+
+        val historyUrl =
+          controllers.history.routes.SubmittedReturnsController
+            .onPageLoadSingleYear("2026")
+            .url
 
         status(result) mustEqual OK
         contentAsString(result) mustEqual view(model, historyUrl)(request, messages(application)).toString
@@ -139,10 +196,8 @@ class PrintSubmissionDetailsControllerSpec extends SpecBase with MockitoSugar {
         .build()
 
       running(application) {
-
         val request = FakeRequest(GET, printSubmissionDetailsRoute)
-
-        val result = route(application, request).value
+        val result  = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
@@ -156,10 +211,8 @@ class PrintSubmissionDetailsControllerSpec extends SpecBase with MockitoSugar {
       val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
 
       running(application) {
-
         val request = FakeRequest(GET, printSubmissionDetailsRoute)
-
-        val result = route(application, request).value
+        val result  = route(application, request).value
 
         status(result) mustEqual SEE_OTHER
         redirectLocation(result).value mustEqual controllers.routes.JourneyRecoveryController.onPageLoad().url
