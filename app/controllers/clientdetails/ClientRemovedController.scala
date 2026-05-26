49f24df271a4c0d7f0c9720022b963dee6f339ff
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2026-05-26 12:52:39 +0100

    refactor: scalafmtAll

--- app/controllers/clientdetails/ClientRemovedController.scala
+++ app/controllers/clientdetails/ClientRemovedController.scala
@@ -24,16 +24,17 @@ import views.html.clientdetails.ClientRemovedView
 
 import javax.inject.{Inject, Named}
 
-class ClientRemovedController @Inject()(
-                                       override val messagesApi: MessagesApi,
-                                       @Named("AgentIdentifier") identify: IdentifierAction,                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
-                                       val controllerComponents: MessagesControllerComponents,
-                                       view: ClientRemovedView
-                                     ) extends FrontendBaseController with I18nSupport {
+class ClientRemovedController @Inject() (
+  override val messagesApi: MessagesApi,
+  @Named("AgentIdentifier") identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: ClientRemovedView
+) extends FrontendBaseController
+    with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
-    implicit request =>
-      Ok(view())
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    Ok(view())
   }
 }
