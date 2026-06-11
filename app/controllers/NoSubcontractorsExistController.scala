9e76607efa8953b9fcf8526884d8385e228b6250
George Coldham <21260083+GeorgeColdham@users.noreply.github.com>
2026-06-11 13:49:27 +0100

    code linting

--- app/controllers/NoSubcontractorsExistController.scala
+++ app/controllers/NoSubcontractorsExistController.scala
@@ -23,17 +23,17 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import views.html.NoSubcontractorsExistView
 
-class NoSubcontractorsExistController @Inject()(
-                                       override val messagesApi: MessagesApi,
-                                       identify: IdentifierAction,
-                                       getData: DataRetrievalAction,
-                                       requireData: DataRequiredAction,
-                                       val controllerComponents: MessagesControllerComponents,
-                                       view: NoSubcontractorsExistView
-                                     ) extends FrontendBaseController with I18nSupport {
+class NoSubcontractorsExistController @Inject() (
+  override val messagesApi: MessagesApi,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  val controllerComponents: MessagesControllerComponents,
+  view: NoSubcontractorsExistView
+) extends FrontendBaseController
+    with I18nSupport {
 
-  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
-    implicit request =>
-      Ok(view())
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    Ok(view())
   }
 }
