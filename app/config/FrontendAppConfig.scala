36e733b91602d5830c245a1d9a2e90784fdca234
George Coldham <21260083+GeorgeColdham@users.noreply.github.com>
2026-06-12 14:04:52 +0100

    Code Review | Changed link str format
    Signed-off-by: George Coldham <21260083+GeorgeColdham@users.noreply.github.com>

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -123,5 +123,4 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
     s"$cisFrontendBaseUrl$submissionUnsuccessfulCannotResubmitPath?cisId=$cisId"
 
   lazy val cisTypeOfSubcontractorUrl: String = configuration.get[String]("urls.cis-contractor-frontend")
-  lazy val addSubcontractorsUrl: String      = s"$cisTypeOfSubcontractorUrl/add/type-of-subcontractor"
 }
