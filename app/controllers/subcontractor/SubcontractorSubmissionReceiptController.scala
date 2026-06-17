032a5d0bcd02106056ab13e4f0804268e404879c
George Coldham <21260083+GeorgeColdham@users.noreply.github.com>
2026-06-17 15:36:15 +0100

    Base page setup
    Signed-off-by: George Coldham <21260083+GeorgeColdham@users.noreply.github.com>

--- app/controllers/subcontractor/SubcontractorSubmissionReceiptController.scala
+++ app/controllers/subcontractor/SubcontractorSubmissionReceiptController.scala
@@ -36,8 +36,11 @@ class SubcontractorSubmissionReceiptController @Inject() (
 
   private val submissionTime = "12:00"
   private val submissionDate = "18 May 2025"
+  private val contractorName = "John Doe"
+  private val employerRef    = "ABC12345"
+  private val IRNumber       = "123456"
 
   def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
-    Ok(view(submissionTime, submissionDate))
+    Ok(view(submissionTime, submissionDate, contractorName, employerRef, IRNumber))
   }
 }
