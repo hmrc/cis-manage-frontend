4adf4f3f49e7cf49e609bf18277b454263f19e8f
George Coldham <21260083+GeorgeColdham@users.noreply.github.com>
2026-06-17 14:15:38 +0100

    Page skeleton setup
    Signed-off-by: George Coldham <21260083+GeorgeColdham@users.noreply.github.com>

---
+++ test/controllers/subcontractor/SubcontractorSubmissionReceiptControllerSpec.scala
@@ -0,0 +1,29 @@
+package controllers.subcontractor
+
+import base.SpecBase
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import views.html.subcontractor.SubcontractorSubmissionReceiptView
+
+class SubcontractorSubmissionReceiptControllerSpec extends SpecBase {
+
+  "SubcontractorSubmissionReceipt Controller" - {
+
+    "must return OK and the correct view for a GET" in {
+
+      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+
+      running(application) {
+        val request =
+          FakeRequest(GET, controllers.subcontractor.routes.SubcontractorSubmissionReceiptController.onPageLoad().url)
+
+        val result = route(application, request).value
+
+        val view = application.injector.instanceOf[SubcontractorSubmissionReceiptView]
+
+        status(result) mustEqual OK
+        contentAsString(result) mustEqual view()(request, messages(application)).toString
+      }
+    }
+  }
+}
