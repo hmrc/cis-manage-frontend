22fd19d175ff60df3454ed7e63f51c60b117c7ef
codeneto-hmrc <253322371+codeneto-hmrc@users.noreply.github.com>
2026-07-03 09:35:26 +0100

    DTR-5924: fix dead link

--- test/views/SubcontractorsLandingPageViewSpec.scala
+++ test/views/SubcontractorsLandingPageViewSpec.scala
@@ -53,6 +53,11 @@ class SubcontractorsLandingPageViewSpec extends SpecBase {
       doc.getElementsByClass("govuk-link").text must include(messages("subcontractorsLandingPage.aside.link1"))
       doc.getElementsByClass("govuk-link").text must include(messages("subcontractorsLandingPage.aside.link2"))
       doc.getElementsByClass("govuk-link").text must include(messages("subcontractorsLandingPage.aside.link3"))
+      doc
+        .select(
+          s"a[href='${controllers.subcontractors.routes.GetSubcontractorListController.onPageLoad().url}']"
+        )
+        .text                                   must include(messages("subcontractorsLandingPage.subcontractorList"))
     }
   }
 
