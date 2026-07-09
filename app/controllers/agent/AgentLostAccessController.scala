887456a33bb9ff170a03fc86948c0f6dc85799a2
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2026-07-09 10:31:54 +0100

    Add(DTR-6726): Initial Scaffolding

---
+++ app/controllers/agent/AgentLostAccessController.scala
@@ -0,0 +1,24 @@
+package controllers.agent
+
+import controllers.actions.*
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.agent.AgentLostAccessView
+
+import javax.inject.{Inject, Named}
+
+class AgentLostAccessController @Inject()(
+                                       override val messagesApi: MessagesApi,
+                                       @Named("AgentIdentifier") identify: IdentifierAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       view: AgentLostAccessView
+                                     ) extends FrontendBaseController with I18nSupport {
+
+  def onPageLoad: Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      Ok(view())
+  }
+}
