83fc34ba3cd1fdb3f161d8caeb2fbc07d1b1a31a
Jesse-Murray-Dev <209797357+Jesse-Murray-Dev@users.noreply.github.com>
2026-08-06 09:08:18 +0100

    DTR-7045: Moved Setup Trait outside of test loop

--- test/views/JourneyRecoveryStartAgainViewSpec.scala
+++ test/views/JourneyRecoveryStartAgainViewSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 HM Revenue & Customs
+ * Copyright 2026 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,19 +49,18 @@ class JourneyRecoveryStartAgainViewSpec extends SpecBase with Matchers {
           doc.getElementsByClass("govuk-link").text must include(
             messages("journeyRecovery.startAgain.guidance.cisAccount.link")
           )
-
         }
       }
-
-      trait Setup {
-        val app                                       = applicationBuilder().build()
-        val view                                      = app.injector.instanceOf[JourneyRecoveryStartAgainView]
-        implicit val request: play.api.mvc.Request[_] = FakeRequest()
-        implicit val messages: Messages               = play.api.i18n.MessagesImpl(
-          play.api.i18n.Lang.defaultLang,
-          app.injector.instanceOf[play.api.i18n.MessagesApi]
-        )
-      }
     }
+  }
+
+  trait Setup {
+    val app                                       = applicationBuilder().build()
+    val view                                      = app.injector.instanceOf[JourneyRecoveryStartAgainView]
+    implicit val request: play.api.mvc.Request[_] = FakeRequest()
+    implicit val messages: Messages               = play.api.i18n.MessagesImpl(
+      play.api.i18n.Lang.defaultLang,
+      app.injector.instanceOf[play.api.i18n.MessagesApi]
+    )
   }
 }
